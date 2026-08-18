aec75176a827f961a36ee03ba5e1b618ffcf0206
sirdavid666 <olujinmiewaoluwa@gmail.com>
2026-08-19 00:59:10 +0100

    Add Flutter engine configuration to MainActivity

--- android/app/src/main/kotlin/com/dave/ai/jarvis/MainActivity.kt
+++ android/app/src/main/kotlin/com/dave/ai/jarvis/MainActivity.kt
@@ -1,6 +1,11 @@
-package com.dave.ai.jarvis
+ package com.dave.ai.jarvis
 
-    import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-    class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
+}
